e9edfdb3c7c4e7b88fd77e96f6c7e2a3e208bc44
Vikram Basu <gaju123@gmail.com>
2017-10-09 16:20:42 +0530

    refactored code to make it much more readable

--- keymatch.py
+++ keymatch.py
@@ -16,9 +16,9 @@ from subprocess import Popen, PIPE, call
 import rarfile
 
 # Bro Locations
-noticelog = '/usr/local/seceon/bro/logs/current/notice.log'
-keywords = '/usr/local/seceon/bro/share/bro/site/file-extraction/kwlist.txt'
-filelocation = '/usr/local/seceon/bro/logs/current/extract_files'
+noticelog = 'notice.log'  # '/usr/local/seceon/bro/logs/current/notice.log'
+keywords = 'testing/kwlist.txt'  # '/usr/local/seceon/bro/share/bro/site/file-extraction/kwlist.txt'
+filelocation = '/home/vikram/PycharmProjects/keywordmatch/testing'  # '/usr/local/seceon/bro/logs/current/extract_files'
 
 # Get all keywords to be found
 f = open(keywords, 'r')
@@ -41,9 +41,10 @@ data['note'] = 'KeywordMatch::Matched'
 fext = os.path.splitext(filename)[1]
 
 # Global variables
-complex_extensions = ['.pdf', '.docx']
-plaintext_extensions = ['.txt', '.doc', '.html', 'htm', 'rtf', 'xml', 'xls', 'json']
-supported_extensions = complex_extensions + plaintext_extensions
+complex_extensions = ['.docx']
+simple_extensions = ['.pdf', '.txt', '.doc', '.html', 'htm', 'rtf', 'xml', 'xls', 'json']
+compressed_extensions = ['.tar', '.gz', '.zip', 'rar']
+supported_extensions = complex_extensions + simple_extensions
 FNULL = open(os.devnull, 'w')
 
 
@@ -62,18 +63,6 @@ def notice_printer(filename):
             time.sleep(0.5)
 
 
-def extension_pdf(filename):
-    for key in keys:
-        call(['pdftotext', '%s' % filename])
-        new_fn = os.path.splitext(filename)
-        new_fn = new_fn[0] + '.txt'
-        pdf_res = call(['grep', '-ic', '%s' % key, '%s' % new_fn], stdout=FNULL)
-        if pdf_res == 0:
-            notice_printer(filename)
-            break
-    os.remove(new_fn)
-
-
 def extension_docx(filename):
     docxread = Popen(['docx2txt', '%s' % filename], stdout=PIPE).communicate()[0]
     for key in keys:
@@ -82,116 +71,69 @@ def extension_docx(filename):
             break
 
 
-def extension_txt(filename):
+def search_text(fname, ext):
+    if ext == '.pdf':
+        call(['pdftotext', '%s' % fname])
+        new_fn = os.path.splitext(fname)
+        fname = new_fn[0] + '.txt'
+    var = ''
     for key in keys:
-        txt_result = call(['grep', '-ic', '%s' % key, '%s' % filename], stdout=FNULL)
-        if txt_result == 0:
-            notice_printer(filename)
-            break
+        var = '-e ' + key + ' ' + var
+    cmd = ('grep -ic %s %s' % (var, fname)).split()
+    txt_result = call(cmd, stdout=FNULL)
+    if txt_result == 0:
+        notice_printer(fname)
+    if ext == 'pdf':
+        os.remove(fname)
 
 
-def extension_tar(filename):
-    tar = tarfile.open(filename)
-    for member in tar.getmembers():
-        if os.path.splitext(member.name)[1] in supported_extensions:
-            tar.extract(member, path='tar')
-    tar.close()
-    ext_tar = glob.glob("tar/*")
-    for files in ext_tar:
-        fn_tar = os.path.basename(files)
-        fext_tar = os.path.splitext(files)[1]
-        os.chdir('tar')
-        if fext_tar.lower() == '.pdf':
-            extension_pdf(fn_tar)
-        if fext_tar.lower() == '.docx':
-            extension_docx(fn_tar)
-        if fext_tar.lower() in plaintext_extensions:
-            extension_txt(fn_tar)
+def decompress(fname, ext):
+    if ext == '.tar':
+        fp = tarfile.open(fname)
+        for member in fp.getmembers():
+            if os.path.splitext(member.name)[1] in supported_extensions:
+                fp.extract(member, path='tmp')
+        fp.close()
+    elif ext == '.gz':
+        fp = tarfile.open(fname, 'r:gz')
+        for member in fp.getmembers():
+            if os.path.splitext(member.name)[1] in supported_extensions:
+                fp.extract(member, path='tmp')
+        fp.close()
+    elif ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+        for member in fp.namelist():
+            if os.path.splitext(member)[1] in supported_extensions:
+                fp.extract(member, path='tmp')
+        fp.close()
+    elif ext == '.rar':
+        fp = rarfile.RarFile(fname)
+        for member in fp.namelist():
+            if os.path.splitext(member)[1] in supported_extensions:
+                fp.extract(member, path='tmp')
+        fp.close()
+    ext_files = glob.glob('tmp/*')
+    for files in ext_files:
+        fn_cmp = os.path.basename(files)
+        fext_cmp = os.path.splitext(files)[1]
+        os.chdir('tmp')
+        if fext_cmp.lower() == '.docx':
+            extension_docx(fn_cmp)
+        elif fext_cmp.lower() in simple_extensions:
+            search_text(fn_cmp, fext_cmp)
         os.chdir('..')
-    if os.path.isdir('tar'):
-        shutil.rmtree('tar')
-
-
-def extension_targz(filename):
-    targz = tarfile.open(filename, 'r:gz')
-    for member in targz.getmembers():
-        if os.path.splitext(member.name)[1] in supported_extensions:
-            targz.extract(member, path='gz')
-    targz.close()
-    ext_targz = glob.glob("gz/*")
-    for files in ext_targz:
-        fn_targz = os.path.basename(files)
-        fext_targz = os.path.splitext(files)[1]
-        os.chdir('gz')
-        if fext_targz.lower() == '.pdf':
-            extension_pdf(fn_targz)
-        if fext_targz.lower() == '.docx':
-            extension_docx(fn_targz)
-        if fext_targz.lower() in plaintext_extensions:
-            extension_txt(fn_targz)
-        os.chdir('..')
-    if os.path.isdir('gz'):
-        shutil.rmtree('gz')
-
-
-def extension_zip(filename):
-    zip_file = zipfile.ZipFile(filename, 'r')
-    for member in zip_file.namelist():
-        if os.path.splitext(member)[1] in supported_extensions:
-            zip_file.extract(member, path='zip')
-    ext_zip = glob.glob("zip/*")
-    for files in ext_zip:
-        fn_zip = os.path.basename(files)
-        fext_zip = os.path.splitext(files)[1]
-        os.chdir('zip')
-        if fext_zip.lower() == '.pdf':
-            extension_pdf(fn_zip)
-        if fext_zip.lower() == '.docx':
-            extension_docx(fn_zip)
-        if fext_zip.lower() in plaintext_extensions:
-            extension_txt(fn_zip)
-        os.chdir('..')
-    if os.path.isdir('zip'):
-        shutil.rmtree('zip')
-
-
-def extension_rar(filename):
-    rar_file = rarfile.RarFile(filename)
-    for member in rar_file.namelist():
-        if os.path.splitext(member)[1] in supported_extensions:
-            rar_file.extract(member, path='rar')
-    ext_rar = glob.glob("rar/*")
-    for files in ext_rar:
-        fn_rar = os.path.basename(files)
-        fext_rar = os.path.splitext(files)[1]
-        os.chdir('rar')
-        if fext_rar.lower() == '.pdf':
-            extension_pdf(fn_rar)
-        if fext_rar.lower() == '.docx':
-            extension_docx(fn_rar)
-        if fext_rar.lower() in plaintext_extensions:
-            extension_txt(fn_rar)
-        os.chdir('..')
-    if os.path.isdir('rar'):
-        shutil.rmtree('rar')
+    if os.path.isdir('tmp'):
+        shutil.rmtree('tmp')
 
 
 def main():
     os.chdir(filelocation)
-    if filename.endswith('.pdf'):
-        extension_pdf(filename)
-    elif filename.endswith('.docx'):
+    if fext.lower() in simple_extensions:
+        search_text(filename, fext)
+    elif fext.lower() in complex_extensions:
         extension_docx(filename)
-    elif filename.endswith('.gz'):
-        extension_targz(filename)
-    elif filename.endswith('.tar'):
-        extension_tar(filename)
-    elif filename.endswith('.zip'):
-        extension_zip(filename)
-    elif filename.endswith('.rar'):
-        extension_rar(filename)
-    elif fext.lower() in plaintext_extensions:
-        extension_txt(filename)
+    elif fext.lower() in compressed_extensions:
+        decompress(filename, fext)
     os.remove(filename)
 
 
